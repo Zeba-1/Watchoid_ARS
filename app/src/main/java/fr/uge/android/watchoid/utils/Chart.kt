bbdb10795e9a0f2f0bbe84b7792e11df12205f92
Zeba <sebsautier18@gmail.com>
2025-03-23 17:46:16 +0100

    add line to better judge response time

--- app/src/main/java/fr/uge/android/watchoid/utils/Chart.kt
+++ app/src/main/java/fr/uge/android/watchoid/utils/Chart.kt
@@ -1,5 +1,6 @@
 package fr.uge.android.watchoid.utils
 
+import android.text.TextPaint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -7,8 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.uge.android.watchoid.entity.report.TestReport
 
 @Composable
@@ -22,7 +27,22 @@ fun TestReportChart(reports: List<TestReport>) {
     ) {
         val barWidth = size.width / (reports.size * 2)
         val maxBarHeight = size.height
+        val textPaint = TextPaint().apply {
+            color = Color.Black.toArgb()
+            textSize = 12.sp.toPx()
+        }
 
+        // Draw horizontal lines and labels
+        for (i in 0..4) {
+            val y = i * maxBarHeight / 4
+            drawLine(
+                color = Color.Gray,
+                start = androidx.compose.ui.geometry.Offset(0f, y),
+                end = androidx.compose.ui.geometry.Offset(size.width, y)
+            )
+        }
+
+        // Draw bars
         reports.forEachIndexed { index, report ->
             val barHeight = (report.responseTime.toFloat() / maxResponseTime) * maxBarHeight
             val xOffset = index * 2 * barWidth
@@ -33,6 +53,16 @@ fun TestReportChart(reports: List<TestReport>) {
                     size = Size(barWidth, barHeight)
                 )
             }
+        }
+
+        for (i in 0..4) {
+            val y = i * maxBarHeight / 4
+            drawContext.canvas.nativeCanvas.drawText(
+                "${maxResponseTime * (4 - i) / 4}",
+                0f,
+                y + textPaint.textSize / 2,
+                textPaint
+            )
         }
     }
 }
